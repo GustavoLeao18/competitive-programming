79d007187fb0ba6caa965ce7e171ee6a6f1c0e56
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-05 09:28:44 -0300

    1035 / 36/ 37/ 38/

--- beecrowd/1037.cpp
+++ beecrowd/1037.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 int main() {
 
+    double x;
     
-
+    cin >> x;
+    
+    if (x < 0 || x > 100) {
+        cout << "Fora de intervalo" << '\n';
+    }
+    else if (x <= 25.00) {
+        cout << "Intervalo [0,25]" << '\n';
+    }
+    else if (x <= 50.00) {
+        cout << "Intervalo (25,50]" << '\n';
+    }
+    else if (x <= 75.00) {
+        cout << "Intervalo (50,75]" << '\n';
+    }
+    else {
+        cout << "Intervalo (75,100]" << '\n';
+    }
+    
     return 0;
 }
--- beecrowd/1038.cpp
+++ beecrowd/1038.cpp
@@ -4,7 +4,28 @@ using namespace std;
 
 int main() {
 
+    int codigo, quant;
+    double total;
     
+    cin >> codigo >> quant;
+    
+    if (codigo == 1) {
+        total = quant * 4;
+    }
+    else if (codigo == 2) {
+        total = quant * 4.5;
+    }
+    else if (codigo == 3) {
+        total = quant * 5;
+    }
+    else if (codigo ==4) {
+        total = quant * 2;
+    }
+    else {
+        total = quant * 1.5;
+    }
+    
+    cout << "Total: R$ " << fixed << setprecision(2) << total << '\n';
 
     return 0;
 }
