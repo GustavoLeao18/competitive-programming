6c406f268812e2cb848d0bc08ba9113e842103be
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-07 09:49:17 -0300

    1071

--- beecrowd/1071.cpp
+++ beecrowd/1071.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main() {
 
+    int x, y, soma, troca;
     
+    cin >> x >> y;
+    
+    if (x > y) {
+        troca = x;
+        x = y;
+        y = troca;
+    }
+    
+    soma = 0;
+    for (int i=x+1; i<y; i++) {
+        if (i % 2 != 0) {
+            soma = soma + i;
+        }
+    }
+    
+    cout << soma << '\n';
     
     return 0;
 }
