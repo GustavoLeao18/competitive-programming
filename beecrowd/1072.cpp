c6b2433d6537b21bbd9c4b08c64d493107574dd3
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-07 10:07:35 -0300

    1072

--- beecrowd/1072.cpp
+++ beecrowd/1072.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 int main() {
 
+    int n, x, in, out;
     
+    cin >> n;
+    
+    in = 0;
+    out = 0;
+    for (int i=0; i<n; i++) {
+        cin >> x;
+        
+        if (x >= 10 && x <= 20) {
+            in++;
+        }
+        else {
+            out++;
+        }
+    }
+    
+    cout << in << " in" << '\n';
+    cout << out << " out" << '\n';
     
     return 0;
 }
