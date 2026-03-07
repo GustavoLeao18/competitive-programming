81d41360db88a94f1bbf490b540b331f809978e2
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-07 10:15:59 -0300

    1073

--- beecrowd/1073.cpp
+++ beecrowd/1073.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+
 int main() {
 
+    int n, quadrado;
     
+    cin >> n;
+    
+    for (int i=1; i<=n; i++) {
+        if (i % 2 == 0) {
+            quadrado = i * i;
+            cout << i << "^2 = " << quadrado << '\n'; 
+        }
+    }
     
     return 0;
 }
