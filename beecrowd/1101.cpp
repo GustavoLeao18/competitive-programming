8519ff4c578977342890d5d805eab2226b47bc58
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-09 10:56:00 -0300

    1101

--- beecrowd/1101.cpp
+++ beecrowd/1101.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 int main() {
 
+    int m, n, soma, troca;
     
+    cin >> m >> n;
+    
+    
+    while (m != 0 && m > 0 && n !=0 && n > 0) {
+        
+    if (m > n) {
+        troca = m;
+        m = n;
+        n =  troca;
+    }
+    
+    soma = 0;
+    
+        for (int i=m; i<=n; i++) {
+            soma = soma + i;
+            cout << i << " ";
+        }
+        cout << "Sum=" << soma << '\n';
+        cin >> m >> n;
+    }
     
     return 0;
 }
