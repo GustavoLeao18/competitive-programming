e2438d4346d53fd2bb350c43a3a3b8324f3145a2
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-09 10:44:33 -0300

    1098

--- beecrowd/1098.cpp
+++ beecrowd/1098.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main() {
 
-    
+    double j;
+
+    j = 1;
+    for (double i=0; i<=2; i+=0.2) {
+        cout << "I=" << i << " J=" << j << '\n';
+        cout << "I=" << i << " J=" << j+1 << '\n';
+        cout << "I=" << i << " J=" << j+2 << '\n';
+        
+        j = j + 0.2;
+    }
     
     return 0;
 }
