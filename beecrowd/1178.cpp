9090d87fdd494938f25308365a539a8d3679a48e
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-11 20:41:05 -0300

    1178

--- beecrowd/1178.cpp
+++ beecrowd/1178.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main() {
 
+    double x, vet[100];
+    
+    cin >> x; 
+    
+    vet[0] = x;
+    
+    cout << fixed << setprecision(4);
+    
+    cout << "N[" << 0 << "] = " << vet[0] << '\n';
+    
+    for (int i=1; i<100; i++) {
+        x = x / 2;
+        vet[i] = x;
+        cout << "N[" << i << "] = " << vet[i] << '\n';
+    }
     
     
     return 0;
