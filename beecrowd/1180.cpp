0954d282075602fb2cfe3cdb4def3b18ca558891
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-11 20:58:01 -0300

    1180

--- beecrowd/1180.cpp
+++ beecrowd/1180.cpp
@@ -5,7 +5,27 @@ using namespace std;
 
 int main() {
 
+    int n, menor, posMenor;
     
+    cin >> n;
+    
+    int x[n];
+    
+    cin >> x[0];
+    
+    menor = x[0];
+    posMenor = 0;
+    for (int i=1; i<n; i++) {
+        cin >> x[i];
+        
+        if (x[i] < menor) {
+            menor = x[i];
+            posMenor = i;
+        }
+    }
+    
+    cout << "Menor valor: " << menor << '\n';
+    cout << "Posicao: " << posMenor << '\n';
     
     return 0;
 }
