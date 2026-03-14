1c678979893b08db4034a588f747b58be773c146
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 17:36:45 -0300

    1133

--- beecrowd/1133.cpp
+++ beecrowd/1133.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main() {
 
+    int x, y, troca;
     
+    cin >> x >> y;
+    
+    if (x > y) {
+        troca = x; 
+        x = y;
+        y = troca;
+    }
+    
+    for (int i=x+1; i<y; i++) {
+        if (i % 5 == 2 || i % 5 == 3) {
+            cout << i << '\n';
+        }
+    }
     
     return 0;
 }
