380fd4dd11d402fd5580d1efc7541f88a6534505
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 11:35:26 -0300

    1114

--- beecrowd/1114.cpp
+++ beecrowd/1114.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main() {
 
+    int senha;
     
+    cin >> senha;
+    
+    while (senha != 2002) {
+        cout << "Senha Invalida" << '\n';
+        cin >> senha;
+    }
+    
+    cout << "Acesso Permitido" << '\n';
     
     return 0;
 }
