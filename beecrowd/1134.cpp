dc36935125e938dc41ef2f5f7b9eac18f0a74213
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 17:41:21 -0300

    1134

--- beecrowd/1134.cpp
+++ beecrowd/1134.cpp
@@ -5,7 +5,32 @@ using namespace std;
 
 int main() {
 
+    int alcool = 0, gasolina = 0, diesel = 0, codigo;
     
+    cin >> codigo;
+    
+    while (codigo != 4) {
+        
+        switch (codigo) {
+            case 1: 
+                alcool++;
+                break;
+            case 2:
+                gasolina++;
+                break;
+            case 3:
+                diesel++;
+                break;
+        }
+        
+        cin >> codigo;
+
+    }
+    
+    cout << "MUITO OBRIGADO" << '\n';
+    cout << "Alcool: " << alcool << '\n';
+    cout << "Gasolina: " << gasolina << '\n';
+    cout << "Diesel: " << diesel << '\n';
     
     return 0;
 }
