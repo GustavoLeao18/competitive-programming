93ff31df9582921ae039989237c7b0a75faee061
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 17:30:49 -0300

    1131

--- beecrowd/1131.cpp
+++ beecrowd/1131.cpp
@@ -5,7 +5,46 @@ using namespace std;
 
 int main() {
 
-    
+    int golI, golG, gremio, inter, empate, cont, codigo;
+
+    codigo = 0;
+    gremio = 0;
+    inter = 0;
+    empate = 0;
+    cont = 0;
+
+    while (codigo != 2) {
+        cin >> golI >> golG;
+        cont++;
+
+        if (golI > golG) {
+            inter++;
+        }
+        else if (golG > golI) {
+            gremio++;
+        }
+        else {
+            empate++;
+        }
+
+        cout << "Novo grenal (1-sim 2-nao)" << '\n';
+        cin >> codigo;
+    }
+
+    cout << cont << " grenais" << '\n';
+    cout << "Inter:" << inter << '\n';
+    cout << "Gremio:" << gremio << '\n';
+    cout << "Empates:" << empate << '\n';
+
+    if (gremio > inter) {
+        cout << "Gremio venceu mais" << '\n';
+    }
+    else if (inter > gremio) {
+        cout << "Inter venceu mais" << '\n';
+    }
+    else {
+        cout << "Nao houve vencedor" << '\n';
+    }
     
     return 0;
 }
