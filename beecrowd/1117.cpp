6a5655983832a972326ac9d4405015d3c60cc2ab
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 17:11:03 -0300

    1117

--- beecrowd/1117.cpp
+++ beecrowd/1117.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 int main() {
 
+    double n1, n2, media;
     
+    cin >> n1;
+    
+    while (n1 < 0 || n1 > 10) {
+        cout << "nota invalida" << '\n';
+        cin >> n1;
+    }
+    
+    cin >> n2;
+    
+    while (n2 < 0 || n2 > 10) {
+        cout << "nota invalida" << '\n';
+        cin >> n2;
+    }
+    
+    media = (n1 + n2) / 2;
+    
+    cout << "media = " << fixed << setprecision(2) << media << '\n';
     
     return 0;
 }
