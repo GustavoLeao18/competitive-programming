e39ab0caa959a3a0cf7bcdb5f3fa6111cc10f6d6
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 17:33:38 -0300

    1132

--- beecrowd/1132.cpp
+++ beecrowd/1132.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 int main() {
 
+    int x, y, troca, soma;
     
+    soma = 0;
+    
+    cin >> x >> y;
+    
+    if (x > y) {
+        troca = x;
+        x = y;
+        y = troca;
+    }
+    
+    for (int i=x; i<=y; i++) {
+        if (i % 13 != 0) {
+            soma = soma + i;
+        }
+    }
+    
+    cout << soma << '\n';
     
     return 0;
 }
