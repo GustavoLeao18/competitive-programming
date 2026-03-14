b34317dfea8e42901232bc262d9098258448e80a
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 18:00:12 -0300

    1144

--- beecrowd/1144.cpp
+++ beecrowd/1144.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main() {
 
+    int n, x;
     
+    cin >> n;
+    
+    x = 1;;
+    
+    for (int i=0; i<n; i++) {
+        cout << x << " " << x * x << " " << x * x * x << '\n';
+        cout << x << " " << (x * x) + 1 << " " << (x * x * x) + 1 << '\n';
+        x++;
+    }
     
     return 0;
 }
