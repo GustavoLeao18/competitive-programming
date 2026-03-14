e4e6ed65e2d8937f412b672e709f56fec4aadfef
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-14 11:41:34 -0300

    1116

--- beecrowd/1116.cpp
+++ beecrowd/1116.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int main() {
 
+    int n;
+    double x, y, divisao;
     
+    cin >> n;
+    
+    for (int i=0; i<n; i++) {
+        cin >> x >> y;
+        
+        if (y != 0) {
+            divisao = x / y;
+            cout << fixed << setprecision(1) << divisao << '\n'; 
+        }
+        else {
+            cout << "divisao impossivel" << '\n';
+        }
+    }
     
     return 0;
 }
