5e9f981bc1234f57a0fc26bd14729c1614d5288e
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-15 20:04:43 -0300

    1150

--- beecrowd/1150.cpp
+++ beecrowd/1150.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int main() {
 
+   int x, z, y, soma, cont = 0;
    
+   cin >> x >> z;
+   
+   while (z <= x) {
+       cin >> z;
+   }
+   
+   soma = x;
+   while (soma <= z) {
+       y = x + 1;
+       soma = soma + y;
+       cont++;
+   }
+   
+   cout << cont << '\n';
     
     return 0;
 }
