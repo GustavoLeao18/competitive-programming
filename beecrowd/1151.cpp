c46353ebb428128708c134e1ae0d92546be67749
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-15 20:22:33 -0300

    1151

--- beecrowd/1151.cpp
+++ beecrowd/1151.cpp
@@ -5,6 +5,36 @@ using namespace std;
 
 int main() {
 
+   int x, y, proximo, n;
+   
+   cin >> n;
+   
+   x = 0;
+   y = 1;
+   
+   if (n == 1) {
+       cout << x;
+   }
+   else if (n == 2) {
+       cout << x << " " << y;
+   }
+   else {
+       cout << x << " " << y << " ";
+       for (int i=3; i<=n; i++) {
+           proximo = x + y;
+           cout << proximo;
+           
+           if (i != n) {
+               cout << " ";
+           }
+           else if (i == n) {
+               cout << '\n';
+           }
+           
+           x = y;
+           y = proximo;
+       }
+   }
    
     
     return 0;
