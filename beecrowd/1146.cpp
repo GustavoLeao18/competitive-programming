41dcd39f26f1f9746c19b57e05e7678aa7f98247
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-15 19:31:49 -0300

    1146

--- beecrowd/1146.cpp
+++ beecrowd/1146.cpp
@@ -5,7 +5,23 @@ using namespace std;
 
 int main() {
 
+   int x;
    
+   cin >> x;
+   
+   while (x != 0) {
+       for (int i=1; i<=x; i++) {
+           cout << i;
+           
+           if (i != x) {
+               cout << " ";
+           }
+           else if (i == x) {
+               cout << '\n';
+           }
+       }
+       cin >> x;
+   }
     
     return 0;
 }
