869ebaa19b39e4de61258e7a54bb23a4f1a24132
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-17 07:49:48 -0300

    1159

--- beecrowd/1159.cpp
+++ beecrowd/1159.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main() {
 
+   int x, soma;
    
+   cin >> x;
+   
+   while (x != 0) {
+       if (x % 2 != 0) {
+           x++;
+       }
+       
+       soma = (5 * x) + 20;
+       
+       cout << soma << '\n';
+       
+       cin >> x;
+   }
     
     return 0;
 }
