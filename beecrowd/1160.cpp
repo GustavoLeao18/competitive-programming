264490ccceecea5cbef5d275af5954a81dba9a30
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-17 08:27:05 -0300

    1160

--- beecrowd/1160.cpp
+++ beecrowd/1160.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 int main() {
 
-   
+    int t, anos;
+    long int pa, pb;
+    double g1, g2;
+    
+    cin >> t;
+    
+    for (int i=0; i<t; i++) {
+        cin >> pa >> pb >> g1 >> g2;
+        anos = 0;
+        while (pa <= pb && anos <= 100) {
+            pa = pa + (g1 / 100 * pa);
+            pb = pb + (g2 / 100 * pb);
+            anos++;
+        }
+        
+        if (anos > 100) {
+            cout << "Mais de 1 seculo." << '\n';
+        }
+        else {
+            cout << anos << " anos." << '\n';
+        }
+    }
     
     return 0;
 }
