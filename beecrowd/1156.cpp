0df42f202e3160bb94d1d25808cd8aca22c3aa72
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-17 07:26:48 -0300

    1156

--- beecrowd/1156.cpp
+++ beecrowd/1156.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main() {
 
+   double s, denominador, cont;
    
+   cont = 1;
+   s = 1;
+   
+   for (int i=3; i<=39; i+=2) {
+       denominador = pow(2, cont);
+       s = s + ((double) i / denominador);
+       cont++;
+   }
+   
+   cout << fixed << setprecision(2) << s << '\n';
     
     return 0;
 }
