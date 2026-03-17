e03ab67b2d64bfacbe6ccdb2d16e1cde7c499355
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-17 07:19:40 -0300

    1155

--- beecrowd/1155.cpp
+++ beecrowd/1155.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 int main() {
 
+   double s;
    
+   s = 1;
+   
+   for (int i=2; i<=100; i++) {
+       s = s + ((double) 1 / i);
+   }
+   
+   cout << fixed << setprecision(2) << s << '\n';
     
     return 0;
 }
