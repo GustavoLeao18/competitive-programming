eda258488326b86ce70f16861d3f7e0f05dd820e
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-16 21:11:20 -0300

    1153

--- beecrowd/1153.cpp
+++ beecrowd/1153.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main() {
 
+   int n, produto;
    
+   cin >> n;
+   
+   produto = 1;
+   
+   for (int i=n; i>0; i--) {
+       produto = produto * i;
+   }
+   
+   cout << produto << '\n';
     
     return 0;
 }
