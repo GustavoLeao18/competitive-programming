e9eeca8542c531bfc189f18a1f5debb49215996f
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-16 21:16:46 -0300

    1154

--- beecrowd/1154.cpp
+++ beecrowd/1154.cpp
@@ -5,7 +5,23 @@ using namespace std;
 
 int main() {
 
+   int idade, cont, soma;
+   double media;
    
+   cin >> idade;
+   
+   cont = 0;
+   soma = 0;
+   
+   while (idade > 0) {
+       cont++;
+       soma = soma + idade;
+       cin >> idade;
+   }
+   
+   media = (double) soma / cont;
+   
+   cout << fixed << setprecision(2) << media << '\n';
     
     return 0;
 }
