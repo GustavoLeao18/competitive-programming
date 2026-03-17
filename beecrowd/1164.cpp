25d8ed3316ed2dd4afe32b78bafeda16a05bfde7
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-17 08:36:33 -0300

    1164

--- beecrowd/1164.cpp
+++ beecrowd/1164.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 int main() {
 
+   int n;
+   long int x, soma;
    
+   cin >> n;
+   
+   for (int i=0; i<n; i++) {
+       cin >> x;
+       soma = 0;
+       
+       for (int j=1; j<x; j++) {
+           if (x % j == 0) {
+               soma = soma + j;
+           }
+       }
+       
+       if (soma == x) {
+           cout << x << " eh perfeito" << '\n';
+       }
+       else {
+           cout << x << " nao eh perfeito" << '\n';
+       }
+   }
     
     return 0;
 }
