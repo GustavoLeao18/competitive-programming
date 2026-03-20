f2feb2f438a8a3a29e6b7b51c193fa769ebeefb9
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-20 15:05:27 -0300

    1531

--- beecrowd/1534.cpp
+++ beecrowd/1534.cpp
@@ -5,7 +5,30 @@ using namespace std;
 
 int main() {
 
+    int n, valor;
     
+    while (cin >> n) {
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                
+               if (j == n - 1 - i) {
+                   valor = 2;
+                   cout << valor;
+               }
+               else if (j == i) {
+                   valor = 1;
+                   cout << valor;
+               } 
+               else {
+                   valor = 3;
+                   cout << valor;
+               }
+                   
+            }
+            
+            cout << '\n';
+        }
+    }
     
     return 0;
 }
