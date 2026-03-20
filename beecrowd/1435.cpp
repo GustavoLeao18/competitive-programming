74368eaf0529758eed4dc3b6ad03d4e54d818676
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-20 09:22:33 -0300

    1435

--- beecrowd/1435.cpp
+++ beecrowd/1435.cpp
@@ -5,7 +5,35 @@ using namespace std;
 
 int main() {
 
+    int n;
+    cin >> n;
     
+    while (n != 0) {
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                int dist_topo = i;
+                int dist_esq = j;
+                int dist_fundo = n - 1 - i;
+                int dist_dir = n - 1 - j;
+
+                int valor = 1 + min({dist_topo, dist_fundo, dist_esq, dist_dir});
+
+                if (j == 0) {
+                    printf("%3d", valor);
+                }
+                else {
+                    printf(" %3d", valor);
+                }
+            }
+
+            cout << '\n';
+        }
+
+        cout << '\n';
+
+        cin >> n;
+    }
     
     return 0;
 }
