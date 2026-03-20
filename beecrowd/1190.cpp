c53e3bb89a78e7f8db548fd5bd07a37f97769591
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-20 08:16:09 -0300

    1190

--- beecrowd/1190.cpp
+++ beecrowd/1190.cpp
@@ -5,7 +5,41 @@ using namespace std;
 
 int main() {
 
+    char operacao;
+    cin >> operacao;
     
+    double mat[12][12];
+    for (int i=0; i<12; i++) {
+        for (int j=0; j<12; j++) {
+            cin >> mat[i][j];
+        }
+    }
+    
+    int cont;
+    double soma, media;
+    
+    cont = 0;
+    soma = 0;
+    
+    for (int i=0; i<12; i++) {
+        for (int j=0; j<12; j++) {
+            if (j > i && j > 11 - i) {
+                soma += mat[i][j];
+                cont++;
+            }
+        }
+    }
+    
+    media = soma / cont;
+    
+    cout << fixed << setprecision(1);
+    
+    if (operacao == 'S') {
+        cout << soma << '\n';
+    }
+    else {
+        cout << media << '\n';
+    }
     
     return 0;
 }
