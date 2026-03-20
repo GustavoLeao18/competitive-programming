0f69daaf60bcfcf4388fcb2ba330bdc4463eca26
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-20 09:58:03 -0300

    1478

--- beecrowd/1478.cpp
+++ beecrowd/1478.cpp
@@ -5,7 +5,55 @@ using namespace std;
 
 int main() {
 
+    int n;
+    cin >> n;
     
+    while (n != 0) {
+
+        for (int i=0; i<n; i++) {
+            int valor = 1, valorA = i+1, valorD = 1;
+
+            for (int j=0; j<n; j++) {
+                
+                if (j > i) {
+                    valorD++;
+                    if (j == 0) {
+                        printf("%3d", valorD);
+                    }
+                    else {
+                        printf(" %3d", valorD);
+                    }
+                }
+                else if (j < i) {
+                    if (j == 0) {
+                        printf("%3d", valorA);
+                    }
+                    else {
+                        printf(" %3d", valorA);
+                    }
+                    valorA--;
+                }
+                else {
+                    if (j == 0) {
+                        printf("%3d", valor);
+                    }
+                    else {
+                        printf(" %3d", valor);
+                    }
+                }
+                
+
+            }
+
+            cout << '\n';
+
+        }
+
+        cout << '\n';
+
+        cin >> n;
+
+    }
     
     return 0;
 }
