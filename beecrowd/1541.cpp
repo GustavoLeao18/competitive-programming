1804a75b0a11c916841182981db38ba7fff0f9df
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-22 16:47:45 -0300

    1541

--- beecrowd/1541.cpp
+++ beecrowd/1541.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main() {
 
+    int a, b, c, areaT, novaA, lado;
+
+    cin >> a;
     
+    while (a != 0) {
+        cin >> b >> c;
+        
+        areaT= a * b;
+        novaA = 100 * areaT / c;
+        lado = sqrt(novaA);
+        
+        cout << lado << '\n';
+        
+        cin >> a;
+    }
     
     return 0;
 }
