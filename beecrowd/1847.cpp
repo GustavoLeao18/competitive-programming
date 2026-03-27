d84317286b1146d1432c551e68a9c30e096774ac
GustavoLeao18 <leaogp2606@gmail.com>
2026-03-27 14:59:51 -0300

    1847

--- beecrowd/1847.cpp
+++ beecrowd/1847.cpp
@@ -5,7 +5,43 @@ using namespace std;
 
 int main() {
 
+    int t1, t2, t3;
+    string h, f = ":)", t = ":(";
     
+    cin >> t1 >> t2 >> t3;
+    
+    if (t2 == t1) {
+        if (t3 > t2) {
+            h = f;
+        }
+        else {
+            h = t;
+        }
+    }
+    else if (t2 > t1) {
+        if (t3 == t2 || t3 < t2) {
+            h = t;
+        }
+        else if (t3 > t2 && (t3 - t2) < (t2 - t1)) {
+            h = t;
+        }
+        else {
+            h = f;
+        }
+    }
+    else if (t2 < t1) {
+        if (t3 > t2 || t3 == t2) {
+            h = f;
+        }
+        else if (t3 < t2 && (t2 - t3) < (t1 - t2)) {
+            h = f;
+        }
+        else {
+            h = t;
+        }
+    }
+    
+    cout << h << '\n';
     
     return 0;
 }
